2521d0242ab4f099957f63acbe34aad1374b4731
Elya29 <elya29870@gmail.com>
2021-06-23 16:32:26 +0200

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+This is a Docker Compose demo.
+
+Launch the app in dev mode with the following command :
+```docker-compose -f docker-compose.dev.yml up```
+
+This will launch the server, the client and the API. You can visit the app on your localhost 8080.
+
+Thanks for your review !
---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='https://media.giphy.com/media/cnhpl4IeYgU7MCBdV2/giphy.gif' alt="gif" />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello Docker World !
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
